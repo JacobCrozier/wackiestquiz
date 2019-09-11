402eb18128d0a60cbeda157778fe71403f4cdf81
Elliot Jordan <elliot.jordan@and.digital>
2019-09-11 14:25:17 +0100

    stylez

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import john from './john.gif';
+import pumpkinDance from './giphy.gif';
+import fresh from './fresh.gif';
+import dancing_ki from './dancing_ki.gif';
+import baby from './dancingbaby.gif';
+import dollar from './dollarspindownd.gif';
+
+
 
 function App() {
   return (
@@ -10,6 +18,22 @@ function App() {
           Welcome to the Wackiest Quiz!
         </p>
       </header>
+      <body>
+        <div class="row">
+          <div class="col-3">
+            <img src={john} vspace="30" class="img-fluid"/>
+            <img src={fresh} vspace="40" class="img-fluid"/>
+            <div></div>
+            <img src={dollar} vspace="40" class="img-fluid"/>
+          </div>
+          <div class="col-6">col-6</div>
+          <div class="col-3">
+            <img src={pumpkinDance} vspace="30" class="img-fluid"/>
+            <img src={dancing_ki} vspace="40" class="img-fluid"/>
+            <img src={baby} vspace="40" class="img-fluid"/>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
