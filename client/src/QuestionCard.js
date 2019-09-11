a23595a4e1f8d39037da9a8fb1e265a7e2cc40e8
hamzabusuri <hamzabusuri96@hotmail.com>
2019-09-11 13:59:12 +0100

    yo

--- client/src/QuestionCard.js
+++ client/src/QuestionCard.js
@@ -1,22 +1,71 @@
 import React from 'react';
+import './App.css';
 
 
-function QuestionCard(){
 
-  function handleClick(){
-    alert('yo');
-  }
+class QuestionCard extends React.Component{
+    constructor(){
+        super()
+
+        this.handleQuestion = this.handleQuestion.bind(this);
+        this.handleYes = this.handleYes.bind(this);
+        this.handleNo = this.handleNo.bind(this);
+
+        this.state = {
+            question : "Is it to do with activities?",
+            yesClick: false,
+            noClick: false,
+            pauseState: false,
+          }
 
 
+      }
+
+      handleYes() {
+        this.setState({yesClick: true},
+          function(){
+            this.setState({pauseState:true}, function(){console.log(this.state)}.bind(this));
+            this.handleQuestion();
+          }.bind(this));
+
+          console.log(this.state);
+
+      }
+
+      handleNo() {
+        this.setState({noClick: true},
+          function(){
+            console.log(this.state);
+          }.bind(this));
+
+      }
+
+      handleQuestion(){
+        this.setState({pauseState: false})
+          if(this.state.yesClick === true && this.state.pauseState === false) {
+            this.setState({question: 'is it a sport'});
+            if(this.state.yesClick === true) {
+              this.setState({question: 'does it involve a ball?'});
+            }
+          };
+      }
+
+      componentDidMount() {
+        this.handleQuestion();
+      }
+
+
+render(){
   return(
-    <div>
-    <label> Is it a sport</label>
-    <button className="btn btn-success" onClick={handleClick}> Yes </button>
-    <button className="btn btn-success" onClick={handleClick}> No </button>
-
-
+    <div className="Question-container">
+    <label> {this.state.question} </label>
+      <div className="Button-container">
+        <button className="btn btn-success" onClick={this.handleYes}> Yes </button>
+        <button className="btn btn-success" onClick={this.handleNo}> No </button>
+      </div>
     </div>
-  );
+      );
+  }
 }
 
 export default QuestionCard;
